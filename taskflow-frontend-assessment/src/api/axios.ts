43243891a28883c258a7cc3c9a6ec898c6b6ed71
Erukulla Pravalika <erukullapravalika2911999@gmail.com>
2026-02-08 00:51:26 +0530

    modified axios

--- taskflow-frontend-assessment/src/api/axios.ts
+++ taskflow-frontend-assessment/src/api/axios.ts
@@ -1,11 +1,10 @@
+// src/api/axios.ts
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
-  withCredentials: true,
+export default axios.create({
+  baseURL: "https://taskflow-2-ydrd.onrender.com",
 });
 
-export default api;
 
 
 
